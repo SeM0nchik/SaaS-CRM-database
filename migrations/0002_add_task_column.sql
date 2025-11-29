948eb2bee1e22f6f5b33032138705f4cf0cefda9
SeM0nchik <semone.pavlychev@gmail.com>
2025-11-29 23:20:27 +0300

    refactor old migrations format

--- migrations/0002_add_task_column.sql
+++ migrations/0002_add_task_column.sql
@@ -1,3 +1,4 @@
+BEGIN;
 --Добавим информацию о скрипте миграции
 INSERT INTO schema_migrations(name)
 VALUES ('add_task_timestamps');
@@ -5,3 +6,4 @@ VALUES ('add_task_timestamps');
 --Добавляем время создания задачи и время ее завершения.
 ALTER TABLE task ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT current_timestamp;
 ALTER TABLE task ADD COLUMN completed_at TIMESTAMP;
+COMMIT;
